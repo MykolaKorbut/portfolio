6c3097fa7d8730ae2821eb77b478d434f1ed05d0
mikolakorbut <nikolay.korbut@gmail.com>
2024-09-01 14:38:48 +0300

    Day 22: add titles, descriptions, images and links on the new columns

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit as st
 
 st.set_page_config(layout="wide")
@@ -10,7 +11,7 @@ with col1:
 with col2:
     st.title("Mykola Korbut")
     content = """
-    Hi, I am Mykola! I am a Python beginner. I started learn python with Python Mega Course by Ardit Sulce.
+    Hi, I am Mykola! I am a Python beginner. I started to learn python with Python Mega Course by Ardit Sulce.
     This is my portfolio app. 
     """
     st.info(content)
@@ -19,3 +20,21 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, empty_col, col4 = st.columns([3, 1, 3])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
